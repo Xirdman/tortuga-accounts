7e75817d9f770e34e65d7cbf10d74e0c32be2f1b
Xirdman <Xirdman@mail.ru>
2021-05-11 21:17:58 +0400

    initial commit

---
+++ src/main/java/com/example/tortuga/TortugaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tortuga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortugaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TortugaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tortuga/TortugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tortuga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortugaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
